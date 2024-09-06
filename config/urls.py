9117850a03593acfaa896267d1ca7c48687b11ff
Divij Sharma <divijs75@gmail.com>
2024-09-07 01:57:08 +0530

    fix(env): Debug values accept bool values
    Signed-off-by: Divij Sharma <divijs75@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path(f"{API_BASE_PATH}data/", include("data.urls")),
     path("admin/", admin.site.urls)
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("admin/", admin.site.urls),
 ]
