7bea02c89316a8a77c0631d3f48cbb039d1bbb04
Divij Sharma <divijs75@gmail.com>
2024-09-07 01:57:08 +0530

    fix(env): Debug values accept bool values
    Signed-off-by: Divij Sharma <divijs75@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,6 @@ Description: This file contains the URL patterns for the Django project.
 
 Author: Divij Sharma <divijs75@gmail.com>
 """
-import os
 from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -32,7 +31,5 @@ urlpatterns = [
     path(f"{API_BASE_PATH}live/", include("live.urls")),
     path(f"{API_BASE_PATH}data/", include("data.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("admin/", admin.site.urls),
 ]
-
-if os.environ.get("ENV") == "development":
-    urlpatterns.append(path("admin/", admin.site.urls))
